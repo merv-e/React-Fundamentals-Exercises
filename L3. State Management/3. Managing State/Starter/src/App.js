47ab42f3140aa71398de437e2cb1f4bbabe10c45
merv-e <34655454+merv-e@users.noreply.github.com>
2022-11-16 16:19:25 +0100

    .

--- L3. State Management/3. Managing State/Starter/src/App.js
+++ L3. State Management/3. Managing State/Starter/src/App.js
@@ -6,18 +6,16 @@ import Button from 'react-bootstrap/Button';
 const App = () => {
   // const [] = useState();
   const [likes, setLikes] = useState(0);
-  const numberOfLikes = () => {
-    setLikes(() => {
-      return likes+1
-    })
-  }
+  // const numberOfLikes = () => {
+  //   setLikes(likes+1);
+  // }
 
   return (
     <div className="container">
       <h2>Like this photo!</h2>
       <img src={corgi} alt="Corgi"></img>
-      <p>Amount of likes: </p>
-      <Button className="btn btn-primary" onClick={numberOfLikes}>Likes {likes}</Button>
+      <p>Amount of likes: {likes} </p>
+      <Button className="btn btn-primary" onClick={() => setLikes(likes+1)}>Like </Button>
     </div>
   );
 };
