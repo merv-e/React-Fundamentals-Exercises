962cf7fa93f2a6ed6b61f7ff1fb56aa52d0b6020
merv-e <34655454+merv-e@users.noreply.github.com>
2022-11-18 15:24:31 +0100

    form, inputs and btn

---
+++ L3. State Management/6. All Together/Starter/src/components/Users.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+const Users = () => {
+
+    const handleForm = (event) => {
+        event.preventDefault();
+    };
+
+    const handleChangeFirstName = (e) => {
+        setFirstName(e.target.value);
+    }; 
+    const handleChangeLastName = (e) => {
+        setLastName(e.target.value);
+    }; 
+    const handleChangeUserName = (e) => {
+        setUserName(e.target.value);
+    }; 
+
+    const noInput = () => firstName.length === 0 || lastName.length === 0 || userName.length === 0;
+
+  return (
+    <div>
+        <form onSubmit={handleForm}>
+            <input type="text" placeholder='First Name' value={firstName} onChange={handleChangeFirstName}  />
+            <input type="text" placeholder='Last Name' value={lastName} onChange={handleChangeLastName} />
+            <input type="text" placeholder='User Name' value={userName} onChange={handleChangeUserName} />
+            <button disabled={noInput()} >Add</button> 
+            <p>{userName}</p>
+        </form>
+    </div>
+  )
+}
+
+export default Users
