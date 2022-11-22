7694184c2dd8668de3c246a594626a816904e60f
merv-e <34655454+merv-e@users.noreply.github.com>
2022-11-22 15:11:08 +0100

    cleaning up the side effect

--- L4. Hooks/3. Side Effect Cleanup/Starter/src/ChildComponent.js
+++ L4. Hooks/3. Side Effect Cleanup/Starter/src/ChildComponent.js
@@ -2,10 +2,20 @@ import React, { useState, useEffect } from "react";
 
 function ChildComponent() {
   const [color, setColor] = useState("red");
-
+  
   useEffect(() => {
-    setTimeout(() => setColor("green"), 3000);
-  });
+    let unmounted = false;
+
+    setTimeout(() => {
+      if (!unmounted) {
+        setColor("green");
+      } 
+    }, 3000);
+
+    return () => { 
+      unmounted = true;
+  }
+}, []);
 
   return <p style={{ color }}>{color}</p>;
 }
